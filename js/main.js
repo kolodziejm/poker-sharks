7b9a994a40c34153937517a897461fb9db422880
Marcin Kołodziej <36101983+kolodziejm@users.noreply.github.com>
2018-05-10 19:02:46 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openSideMenu() {
+    document.querySelector('.side-nav').style.width = "22rem";
+}
+
+function closeSideMenu() {
+    // najwyżej dam id do konkretnego button close'a
+    document.querySelector('.side-nav').style.width = "0";
+}
